193d97e922981a161063b33805d4b73b88c266c2
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2020-04-10 16:24:31 -0700

    index page

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')    
+
+def books_index(request):
+    return render(request, 'books/index.html', {'books': book})    
